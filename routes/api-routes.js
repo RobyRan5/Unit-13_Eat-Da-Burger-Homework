5895abd0fdf9dddcf3ed10422c73eab0ba28eb2c
Roby Ran <robyran@icloud.com>
2020-01-23 08:22:25 -0600

    homework phase 4 push

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,19 +1,9 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
 
-// Dependencies
-// =============================================================
-var Character = require("../models/character.js");
+var Character = require("../models/orders.js");
 
-// Routes
-// =============================================================
 module.exports = function(app) {
-  // Search for Specific Character (or all characters) then provides JSON
-  app.get("/api/:characters?", function(req, res) {
+  app.get("/api/:orders?", function(req, res) {
     if (req.params.characters) {
-      // Display the JSON for ONLY that character.
-      // (Note how we're using the ORM here to run our searches)
       Character.findOne({
         where: {
           routeName: req.params.characters
@@ -28,24 +18,14 @@ module.exports = function(app) {
     }
   });
 
-  // If a user sends data to add a new character...
   app.post("/api/new", function(req, res) {
-    // Take the request...
-    var character = req.body;
+    var newOrder = req.body;
 
-    // Create a routeName
-
-    // Using a RegEx Pattern to remove spaces from character.name
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
     var routeName = character.name.replace(/\s+/g, "").toLowerCase();
 
-    // Then add the character to the database using sequelize
-    Character.create({
+    newOrder.create({
       routeName: routeName,
-      name: character.name,
-      role: character.role,
-      age: character.age,
-      forcePoints: character.forcePoints
+      name: newOrder.name,
     });
 
     res.status(204).end();
