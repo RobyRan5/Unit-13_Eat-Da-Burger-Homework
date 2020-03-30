26e7cbf8e7652b15b666a7557782bc55fdf72b86
Roby Ran <robyran@icloud.com>
2020-03-30 17:46:26 -0500

    This is for the final cut

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INFO burger_db.burgers (name, devoured) VALUES ('cheeseburger', true);
+INSERT INFO burger_db.burgers (name, devoured) VALUES ('burger', 'true');
+INSERT INFO burger_db.burgers (name, devoured) VALUES ('doublecheeseburger', 'true');
